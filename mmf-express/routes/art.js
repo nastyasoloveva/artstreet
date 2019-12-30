99413323aa3cd94a9f62d5f94cd83ba60514e3bc
Sergey Harlanov <s.v.harlanov@gmail.com>
2019-12-30 17:53:14 +0300

    upload express

--- mmf-express/routes/art.js
+++ mmf-express/routes/art.js
@@ -26,18 +26,21 @@ router.get('/district', (req, res) => {
 router.get('/object', (req, res) => {
    var object = [ 
        { 
+        "url": "https://myslo.ru/Content/BlogArticle/c6/26/c626188e-0630-4611-adc7-507dc28f9f1c_1.jpg",
         "date": faker.date.past(),
-        "location": faker.address.latitude() + ' ' + faker.address.longitude(),
+        "location": faker.address.latitude() + '' + faker.address.longitude(),
         "author": faker.name.findName()
        }, 
 
        { 
+        "url": "https://zagge.ru/wp-content/uploads/2017/01/Eduardo_Kobra-14.jpg",
         "date": faker.date.past(),
-        "location": faker.address.latitude() + ' ' + faker.address.longitude(),
+        "location": faker.address.latitude() + '' + faker.address.longitude(),
         "author": faker.name.findName() 
        },
 
        {
+        "url": "https://cs7.pikabu.ru/post_img/big/2018/04/15/6/1523783544117659603.jpg",
         "date": faker.date.past(),
         "location": faker.address.latitude() + ' ' + faker.address.longitude(),
         "author": faker.name.findName()
